fac2cab11867e43a321868790e7b450a090ad81b
trip2006edenshige-cmyk <trip2006edenshige@tripjapan.in>
2026-07-16 13:05:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,101 @@
 // script.js
 
+const GAS_URL = "https://script.google.com/macros/s/AKfycbyuYNM-_9U3FAvFMqLZA3G8CKKEl4it6YU4ItMaGkqKjpcPE225hz1KXJ-41laSLvUK/exec";
+
+
 const form = document.getElementById("reservationForm");
+
 const message = document.getElementById("message");
 
 
-form.addEventListener("submit", function(e) {
+
+form.addEventListener("submit", function(e){
 
   e.preventDefault();
 
 
+
   const data = {
 
-    name: document.getElementById("name").value,
+    name:
+    document.getElementById("name").value,
 
-    phone: document.getElementById("phone").value,
 
-    work: document.getElementById("work").value,
+    phone:
+    document.getElementById("phone").value,
 
-    dateTime: document.getElementById("dateTime").value,
 
-    note: document.getElementById("note").value
+    work:
+    document.getElementById("work").value,
+
+
+    dateTime:
+    document.getElementById("dateTime").value,
+
+
+    note:
+    document.getElementById("note").value
 
   };
 
 
-  console.log(data);
+
+  fetch(GAS_URL, {
+
+    method:"POST",
+
+    body:JSON.stringify(data)
+
+  })
 
 
-  message.textContent =
-    "予約を受け付けました。確認後ご連絡いたします。";
+  .then(response => response.json())
 
 
-  message.style.color = "green";
+  .then(result => {
 
 
-  form.reset();
+    if(result.result === "success"){
+
+
+      message.textContent =
+      "予約を受け付けました。";
+
+
+      message.style.color="green";
+
+
+      form.reset();
+
+
+    }else{
+
+
+      message.textContent =
+      "送信エラーが発生しました。";
+
+
+    }
+
+
+  })
+
+
+  .catch(error => {
+
+
+    console.error(error);
+
+
+    message.textContent =
+    "通信エラーです。";
+
+
+    message.style.color="red";
+
+
+  });
+
 
 
 });
